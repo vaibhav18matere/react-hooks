a1124e3d0d5dde37f2fb565453738bdaf2c71289
Vaibhav Matere <vpm00718897@gmail.com>
2023-04-09 20:03:24 +0530

    useState - onInput and onChange

--- src/hooks/UseStateHook.jsx
+++ src/hooks/UseStateHook.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 function UseStateHook() {
   const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("name is");
+
+  const [surname, setSurname] = useState("surname is");
 
   const incrementCountHandler = () => {
     setCounter((count) => count + 1);
@@ -11,11 +14,29 @@ function UseStateHook() {
   //     setCounter((count) => count - 1);
   //   };
 
+  const inputTextHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onChangeHandler = (event) => {
+    setSurname(event.target.value);
+  };
+
   return (
     <>
       <h4>counter : {counter}</h4>
       <button onClick={incrementCountHandler}>Increment</button>
       <button onClick={() => setCounter(counter - 1)}>Decreament</button>
+      <br />
+      <br />
+      <input placeholder="enter name" onInput={(e) => inputTextHandler(e)} />
+      <h3>{inputValue}</h3>
+      <small>1. using "onInput" attribute</small>
+      <br />
+      <br />
+      <input placeholder="enter surname" onChange={onChangeHandler} />
+      <h3>{surname}</h3>
+      <small>2. using "onInput" attribute</small>
     </>
   );
 }
